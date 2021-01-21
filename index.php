9f41605c9fd9ec72b859f960cd52b0dfc7631226
Phelipe Nathan Bastos de Lima <phelipe-nathandf@hotmail.com>
2021-01-21 14:38:02 -0300

    classe Usuario criada

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php  
 
 require_once("config.php");
-
+/*
 $sql = new sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
@@ -9,7 +9,13 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 
 
+*/
 
+$root = new Usuario();
+
+$root->loadById(7);
+
+echo $root;
 
 
 
